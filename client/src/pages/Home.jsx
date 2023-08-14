4535ea5e29790ae5dfa157efe610e45c0bda3566
Hassaanjbaig-code <hassaanb000@gmail.com>
2023-08-14 21:24:30 +0500

    Update the changes to client side

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -27,7 +27,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('http://localhost:8080/api/v1/post', {
+      const response = await fetch('https://ai-image-xkc8.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -60,20 +60,6 @@ const Home = () => {
       }, 500),
     );
   };
-
-//   const handleSearchChange = (e) =>{
-//   setSearchText(e.target.value)
-//   setSearchTimeout(
-//   setTimeout(() => {
-//     const searchResult = allPosts.filter((item) => {
-//       item.name.toLowerCase().includes(searchText.toLowerCase()) || 
-//       item.prompt.toLowerCase().includes(searchText.toLowerCase())
-
-//     })
-//     setSearchedResults(searchResult);
-//   }, 500)
-//   )
-// }
 
 
   return (
